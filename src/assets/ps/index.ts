96d7b7bcc290003bd677ff51556bf354ae9b38ff
morecode <761170536@qq.com>
2025-08-09 17:35:35 +0800

    feat: build

--- src/assets/ps/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './01.png';
-export * from './02.png';
-export * from './03.png';
-export * from './04.png';
-export * from './05.png';
-export * from './06.png';
-export * from './07.png';
-export * from './08.png';
-
