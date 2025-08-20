48f15b32718dfa89ea4e72d7da82bc57271f61fc
morecode <761170536@qq.com>
2025-08-20 22:38:07 +0800

    feat: 素材更新

--- src/pages/our-story/index.tsx
+++ src/pages/our-story/index.tsx
@@ -2,6 +2,28 @@ import { useState, useEffect } from 'react';
 import styles from './index.module.css';
 import FallbackLightbox, { PhotoItem } from '../../components/FallbackLightbox';
 
+const dailyPhotos = [
+  'https://wx-love-img.afunapp.com/FhhS5m0e0215GRMAUCS97_Y5uauw',
+  'https://wx-love-img.afunapp.com/FjQsYlcoo4Q8KqOjVIXp-XD47vdm',
+  'https://wx-love-img.afunapp.com/FkjxPmCImGp0W5qzOvt6uzjZAqwL',
+  'https://wx-love-img.afunapp.com/FhrDy1onJh8JrYqDPS6-YpHViZHd',
+  'https://wx-love-img.afunapp.com/lkl9HLJKrBK5qGRkl6_Hafqd0i42',
+  'https://wx-love-img.afunapp.com/FjCU_UrGMa7Pexc6fGLuBOKPYHtU',
+  'https://wx-love-img.afunapp.com/Fuhvq2i8FNbMO6j6ZUtiLtGOoOoJ',
+  'https://wx-love-img.afunapp.com/FnbIkmmiVMyQh5ax1qG3f5V5HPig',
+  'https://wx-love-img.afunapp.com/Fg3z69kHqRMIjRLzcy8uHPvEH81u',
+  'https://wx-love-img.afunapp.com/lkjz4KyPgXVEBP6yXQ96ReWK92Jo',
+  'https://wx-love-img.afunapp.com/lugofrqiXXjokTrs7hbOowyISItQ',
+  'https://wx-love-img.afunapp.com/FldndRwcnllinoRoZDz8A-XFonHD',
+  'https://wx-love-img.afunapp.com/FrWRgxh0a0d2zNU1i1UdpwUjuyNL',
+  'https://wx-love-img.afunapp.com/lt_Q6vCAR6UZyj5bZcBG8ED37L6N',
+  'https://wx-love-img.afunapp.com/Fl__JYVGYMdWDr3X3JYHcAByQ2IT',
+  'https://wx-love-img.afunapp.com/Fr4uZWT9yooGqWEoUPgB2n1xZRBH',
+  'https://wx-love-img.afunapp.com/FpTLiHbxDKI-HrnLQHvDUTd6-dE7',
+  'https://wx-love-img.afunapp.com/FvJLcQ9A1u1yroCwBqqDSBvfLA2c',
+  'https://wx-love-img.afunapp.com/FhJCAEUQOPNwe6EETNn1JpUGwt8k',
+];
+
 export default function OurStoryPage() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [isLightboxOpen, setIsLightboxOpen] = useState(false);
@@ -10,39 +32,6 @@ export default function OurStoryPage() {
   useEffect(() => {
     setIsLoaded(true);
   }, []);
-
-  const dailyPhotos: PhotoItem[] = [
-    {
-      id: 1,
-      title: '日常照片1',
-      description: '美好的回忆',
-      src: 'https://wx-love-img.afunapp.com/FrA_6hxQPjO-lvYtInfw_JCkUHpi',
-    },
-    {
-      id: 2,
-      title: '日常照片2',
-      description: '甜蜜时光',
-      src: 'https://wx-love-img.afunapp.com/FoCQ5usVUC9dgprLjI6MoQhsiuhJ',
-    },
-    {
-      id: 3,
-      title: '日常照片3',
-      description: '幸福瞬间',
-      src: 'https://wx-love-img.afunapp.com/Fsf3jZo4Ay7W08_AL9okYvLaR9rX',
-    },
-    {
-      id: 4,
-      title: '日常照片4',
-      description: '温馨时刻',
-      src: 'https://wx-love-img.afunapp.com/FvPmOCoiFk3p7sOVdquYBay00Eol',
-    },
-    {
-      id: 5,
-      title: '日常照片5',
-      description: '珍贵回忆',
-      src: 'https://wx-love-img.afunapp.com/FpDRj3TuNnAIRF6gan1CQuskscET',
-    },
-  ];
 
   const handlePhotoClick = (index: number) => {
     setCurrentPhotoIndex(index);
@@ -60,28 +49,15 @@ export default function OurStoryPage() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h1 className={`${styles.title} ${isLoaded ? styles.loaded : ''}`}>
-          恋爱日常
-        </h1>
-        <p className={`${styles.subtitle} ${isLoaded ? styles.loaded : ''}`}>
-          记录我们在一起的每一个美好瞬间
-        </p>
+        <h1 className={`${styles.title} ${isLoaded ? styles.loaded : ''}`}>恋爱日常</h1>
+        <p className={`${styles.subtitle} ${isLoaded ? styles.loaded : ''}`}>记录我们在一起的每一个美好瞬间</p>
       </div>
 
       <div className={styles.content}>
         <div className={`${styles.photoGrid} ${isLoaded ? styles.loaded : ''}`}>
-          {dailyPhotos.map((photo, index) => (
-            <div
-              key={photo.id}
-              className={styles.photoCard}
-              style={{ animationDelay: `${index * 0.1}s` }}
-              onClick={() => handlePhotoClick(index)}
-            >
-              <img
-                src={photo.src}
-                alt={photo.title}
-                className={styles.photo}
-              />
+          {dailyPhotos.map((url, index) => (
+            <div className={styles.photoCard} style={{ animationDelay: `${index * 0.1}s` }} onClick={() => handlePhotoClick(index)}>
+              <img src={url} className={styles.photo} />
             </div>
           ))}
         </div>
@@ -91,7 +67,7 @@ export default function OurStoryPage() {
         isOpen={isLightboxOpen}
         onClose={handleCloseLightbox}
         currentPhoto={currentPhotoIndex}
-        photos={dailyPhotos}
+        photos={dailyPhotos.map((url, index) => ({ id: index, title: `日常照片${index + 1}`, description: '珍贵回忆', src: url }))}
         onPhotoChange={handlePhotoChange}
       />
 
@@ -101,4 +77,3 @@ export default function OurStoryPage() {
     </div>
   );
 }
-
